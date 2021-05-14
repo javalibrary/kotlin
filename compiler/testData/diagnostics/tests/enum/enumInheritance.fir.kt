bfdf7369a1f33599b47f499248a8d293ace2fd6f
Denis.Zharkov <denis.zharkov@jetbrains.com>
2021-05-14 14:39:24 +0300

    FIR: Properly deserialize upper bounds of classes type parameters
    ^KT-46661 Fixed

--- compiler/testData/diagnostics/tests/enum/enumInheritance.fir.kt
+++ compiler/testData/diagnostics/tests/enum/enumInheritance.fir.kt
@@ -1,7 +1,7 @@
 // FILE: test.kt
 enum class MyEnum(): <!CLASS_IN_SUPERTYPE_FOR_ENUM!>MyClass<!>() {}
 enum class MyEnum2(): MyTrait {}
-enum class MyEnum3(): <!CLASS_IN_SUPERTYPE_FOR_ENUM, FINAL_SUPERTYPE!>MyEnumBase<!>() {}
+enum class MyEnum3(): <!CLASS_IN_SUPERTYPE_FOR_ENUM, FINAL_SUPERTYPE, UPPER_BOUND_VIOLATED!>MyEnumBase<!>() {}
 
 open class MyClass() {}
 
