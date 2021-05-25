f4f1847108e1a32a1e8f45377fcb164758f6ea4c
Denis.Zharkov <denis.zharkov@jetbrains.com>
2021-05-25 14:56:15 +0300

    ~ remove directive

--- compiler/testData/diagnostics/tests/delegatedProperty/inference/extensionProperty.kt
+++ compiler/testData/diagnostics/tests/delegatedProperty/inference/extensionProperty.kt
@@ -4,10 +4,10 @@ package foo
 import kotlin.reflect.KProperty
 
 open class A {
-    val B.w: Int by <!DELEGATE_SPECIAL_FUNCTION_NONE_APPLICABLE!>MyProperty()<!>
+    val B.w: Int by <!DELEGATE_SPECIAL_FUNCTION_NONE_APPLICABLE, NEW_INFERENCE_ERROR!>MyProperty()<!>
 }
 
-val B.r: Int by <!DELEGATE_SPECIAL_FUNCTION_NONE_APPLICABLE!>MyProperty()<!>
+val B.r: Int by <!DELEGATE_SPECIAL_FUNCTION_NONE_APPLICABLE, NEW_INFERENCE_ERROR!>MyProperty()<!>
 
 val A.e: Int by MyProperty()
 
